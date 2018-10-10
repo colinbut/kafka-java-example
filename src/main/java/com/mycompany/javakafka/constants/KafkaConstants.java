3c715032e501687756f95aa5011ae7d727c72548
Colin But <colinbut@users.noreply.github.com>
2018-10-10 22:06:38 +0100

    Configuration details

---
+++ src/main/java/com/mycompany/javakafka/constants/KafkaConstants.java
@@ -0,0 +1,16 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.javakafka.constants;
+
+public final class KafkaConstants {
+
+    private KafkaConstants(){}
+
+    public static final String TOPIC = "kafka-java-topic";
+
+    // Kafka Brokers
+    public static final String BOOTSTRAP_SERVERS = "localhost:9092, localhost:9093, localhost:9094";
+}
