066cc9aba25107e516a9f0f1da261bdcbc031fd0
Colin But <colinbut@users.noreply.github.com>
2018-10-18 22:09:45 +0100

    added Consumer config

--- src/main/java/com/mycompany/kafkajava/config/KafkaConfig.java
+++ src/main/java/com/mycompany/kafkajava/config/KafkaConfig.java
@@ -6,12 +6,18 @@
 package com.mycompany.kafkajava.config;
 
 import com.mycompany.kafkajava.constants.KafkaConstants;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Collections;
 import java.util.Properties;
 
 public final class KafkaConfig {
@@ -24,6 +30,17 @@ public final class KafkaConfig {
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaJava");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return new KafkaProducer<Long, String>(properties);
+        return new KafkaProducer<>(properties);
+    }
+
+    public static Consumer<Long, String> getConsumer() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.BOOTSTRAP_SERVERS);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaJava");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        Consumer<Long, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singleton(KafkaConstants.TOPIC));
+        return consumer;
     }
 }
