197fe00a37da1b314107e6ff3a46aceaa68c74d8
Colin But <colinbut@users.noreply.github.com>
2018-10-21 20:02:09 +0100

    Implementing the consumer

--- src/main/java/com/mycompany/kafkajava/App.java
+++ src/main/java/com/mycompany/kafkajava/App.java
@@ -1,8 +1,21 @@
 package com.mycompany.kafkajava;
 
+import com.mycompany.kafkajava.consumer.KafkaMessageConsumer;
+import com.mycompany.kafkajava.producer.KafkaMessageSender;
+
+import java.util.UUID;
+
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main( String[] args ) {
+        final KafkaMessageConsumer kafkaMessageConsumer = new KafkaMessageConsumer();
+        final KafkaMessageSender kafkaMessageSender = new KafkaMessageSender();
+
+        new Thread(kafkaMessageConsumer::consumeMessage).start();
+
+        new Thread(() -> {
+            String message = "Message-" + UUID.randomUUID();
+            kafkaMessageSender.sendMessage(message);
+        }).start();
     }
 }
---
+++ src/main/java/com/mycompany/kafkajava/consumer/KafkaMessageConsumer.java
@@ -0,0 +1,47 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.kafkajava.consumer;
+
+import com.mycompany.kafkajava.config.KafkaConfig;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaMessageConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaMessageConsumer.class);
+
+    private static final int GIVE_UP = 100;
+
+    public void consumeMessage(){
+        final Consumer<Long, String> consumer = KafkaConfig.getConsumer();
+
+        int recordsCount = 0;
+
+        while (true) {
+            ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+
+            if (consumerRecords.count() == 0) {
+                recordsCount++;
+                if (recordsCount > GIVE_UP) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+
+            consumerRecords.forEach(consumerRecord -> LOGGER.info("Consumer Record:(%d %s, %d, %d)%n",
+                consumerRecord.key(), consumerRecord.value(),
+                consumerRecord.partition(), consumerRecord.offset()));
+
+            consumer.commitAsync();
+        }
+
+        consumer.close();
+        LOGGER.info("Done");
+    }
+}
